00f5816eea2ff887a85501fd9af414c10f54caf7
DXdok_zp32@student.itstep.org <DXdok_zp32@student.itstep.org>
2023-11-21 19:52:30 +0200

    Добавьте файлы проекта.

---
+++ DZ_NEW_11_Dedok.cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+    const int numStudents = 5;
+    const int maxLength = 50;
+    char students[numStudents][maxLength];
+
+    cout << "Input 5 surnames of students:\n";
+    for (int i = 0; i < numStudents; ++i) {
+        cout << "Student " << (i + 1) << ": ";
+        cin >> students[i];
+    }
+    for (int i = 0; i < numStudents - 1; ++i) {
+        for (int j = 0; j < numStudents - i - 1; ++j) {
+            if (strcmp(students[j], students[j + 1]) > 0) {
+                char temp[maxLength];
+                strcpy(temp, students[j]);
+                strcpy(students[j], students[j + 1]);
+                strcpy(students[j + 1], temp);
+            }
+        }
+    }
+    cout << "\nSorted surnames:\n";
+    for (int i = 0; i < numStudents; ++i) {
+        cout << students[i] << "\n";
+    }
+
+    return 0;
+}
